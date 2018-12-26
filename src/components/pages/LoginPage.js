630f44103122ea822eaef0c832aac6aaa263ebd3
darrellrizojose <darrell.rizojose@precisiontechpartners.com>
2018-12-26 20:38:35 +0800

    Added loginfrom logic

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import LoginForm from './../forms/LoginForm';
 
-const LoginPage  = () => (
-    <div>
-        <h1>Login Page</h1>
-    </div>
-);
+class LoginPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.props = props;
+    }
+
+    submit = data => {
+        console.log(data);
+    };
+    
+    render() {
+        return (
+            <div>
+                <h1>Login Page</h1>
+                <LoginForm submit={this.submit} />
+            </div>
+        )
+    }
+}
+
 
 export default LoginPage;
